dafa5d0a42bb2d88d549b12eae27746d1845afd0
charank631 <118187972+charank631@users.noreply.github.com>
2023-09-30 20:40:50 +0530

    Add files via upload

--- Hospital Managmet System/hospital/hms/include/checklogin.php
+++ Hospital Managmet System/hospital/hms/include/checklogin.php
@@ -5,7 +5,8 @@ if(strlen($_SESSION['login'])==0)
 	{	
 		$host = $_SERVER['HTTP_HOST'];
 		$uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-		$extra="./user-login.php";		
+		$extra="./index.php";		
+		$_SESSION["login"]="";
 		header("Location: http://$host$uri/$extra");
 	}
 }
--- Hospital Managmet System/hospital/hms/include/sidebar.php
+++ Hospital Managmet System/hospital/hms/include/sidebar.php
@@ -21,23 +21,84 @@
 								</a>
 							</li>
 							<li>
-								<a href="book-appointment.php">
+								<a href="javascript:void(0)">
 									<div class="item-content">
 										<div class="item-media">
-											<i class="ti-pencil-alt"></i>
+											<i class="ti-user"></i>
 										</div>
 										<div class="item-inner">
-											<span class="title"> Book Appointment </span>
+											<span class="title"> Doctors </span><i class="icon-arrow"></i>
 										</div>
 									</div>
 								</a>
-							</li>
+								<ul class="sub-menu">
+									<li>
+										<a href="doctor-specilization.php">
+											<span class="title"> Doctor Specialization </span>
+										</a>
+									</li>
+									<li>
+										<a href="add-doctor.php">
+											<span class="title"> Add Doctor</span>
+										</a>
+									</li>
+									<li>
+										<a href="Manage-doctors.php">
+											<span class="title"> Manage Doctors </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
 
-							<li>
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Users </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-users.php">
+											<span class="title"> Manage Users </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+								<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-user"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Patients </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="manage-patient.php">
+											<span class="title"> Manage Patients </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>	
+
+<li>
 								<a href="appointment-history.php">
 									<div class="item-content">
 										<div class="item-media">
-											<i class="ti-list"></i>
+											<i class="ti-file"></i>
 										</div>
 										<div class="item-inner">
 											<span class="title"> Appointment History </span>
@@ -45,18 +106,101 @@
 									</div>
 								</a>
 							</li>
-<li>
-								<a href="manage-medhistory.php">
+
+
+
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Conatctus Queries </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="unread-queries.php">
+											<span class="title"> Unread Query </span>
+										</a>
+									</li>
+
+									<li>
+										<a href="read-query.php">
+											<span class="title"> Read Query </span>
+										</a>
+									</li>
+									
+								</ul>
+								</li>
+
+
+
+	<li>
+								<a href="doctor-logs.php">
 									<div class="item-content">
 										<div class="item-media">
 											<i class="ti-list"></i>
 										</div>
 										<div class="item-inner">
-											<span class="title"> Medical History </span>
+											<span class="title"> Doctor Session Logs </span>
+										</div>
+									</div>
+								</a>
+							</li>		
+
+
+
+							<li>
+								<a href="user-logs.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-list"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> User Session Logs </span>
+										</div>
+									</div>
+								</a>
+							</li>						
+				<li>
+								<a href="javascript:void(0)">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-files"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Reports </span><i class="icon-arrow"></i>
+										</div>
+									</div>
+								</a>
+								<ul class="sub-menu">
+									
+									<li>
+										<a href="between-dates-reports.php">
+											<span class="title">B/w dates reports </span>
+										</a>
+									</li>
+
+									
+									
+								</ul>
+								<li>
+								<a href="patient-search.php">
+									<div class="item-content">
+										<div class="item-media">
+											<i class="ti-search"></i>
+										</div>
+										<div class="item-inner">
+											<span class="title"> Patient Search </span>
 										</div>
 									</div>
 								</a>
 							</li>
+								</li>
 
 						</ul>
 						<!-- end: CORE FEATURES -->
